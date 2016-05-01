a08b791fb5c7e9f65cc2481c880ec3d10642ad7a
musale <martinmshale>
2016-05-01 20:34:32 +0300

    set up the files and necessary libs

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by musale on 5/1/16.
+ */
+import {Component} from 'angular2/core';
+@Component({
+    selector: 'app',
+    template: `<p>Hello Phillis Dev</p>`
+})
+export class AppComponent {
+    constructor() { }
+}
---
+++ app/bootstrap.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by musale on 5/1/16.
+ */
+import { bootstrap } from 'angular2/platform/browser';
+import {AppComponent} from "./app.component";
+
+bootstrap(AppComponent);
